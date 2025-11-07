5c77b5a40b6dc5756d90952ab76de2239dbda07b
al hussein <wzmh.al@gmail.com>
2025-11-07 17:22:10 +0700

    buat file migrate create tabel pengajar dan siswa

---
+++ database/migrations/2025_11_07_101156_create_siswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->onDelete('cascade');
+            
+            $table->string('nisn')->unique()->nullable()->comment('Nomor Induk Siswa Nasional');
+            $table->text('alamat')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+
+            $table->timestamps();
+
+            $table->unique('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
